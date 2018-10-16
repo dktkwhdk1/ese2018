b158ba1ea1b317da5548714b692562edd4a12122
Heeseock Kang <7yourmom@naver.com>
2018-10-16 00:56:48 -0700

    added turbo on/off

--- myapp.c
+++ myapp.c
@@ -2,10 +2,25 @@
 #include"func1.h"
 #include"func2.h"
 
+void turbo_on()
+{
+	printf("turbo on\n");
+}
+void turbo_off()
+{
+        printf("turbo off\n");
+}
+
+
+
 int main(void)
 {
+	turbo_on();
+
 	foo1();
 	foo2();
+
+	turbo_off();
 
 	return 0;
 }
